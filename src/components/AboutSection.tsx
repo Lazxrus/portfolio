8383bb7d20192c7affcf8aa256fc9ee83d65347f
Lazxrus <Lazxrus@proton.me>
2026-05-02 16:17:51 -0300

    debugging and AboutSection.tsx finished

--- src/components/AboutSection.tsx
+++
@@ -1,3 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { Briefcase, Code, User, Download, Calendar, Sparkles, Target, Github, Linkedin, Twitter, Mail, Star } from 'lucide-react';
-import { motion, AnimatePresence } from 'framer-motion';
